f375154ea2f0290d1549c10bd9bf83e94016d1bd
Dan O'Donovan <danodonovan@users.noreply.github.com>
2013-01-20 19:58:58 +0000

    New model and populate utility - based on CCCBR method archive

---
+++ methods/management/commands/populate.py
@@ -0,0 +1,143 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+import logging
+
+from django.core.management.base import BaseCommand
+
+from methods.models import MethodSet, Method, FirstHandbellPeal, FirstTowerbellPeal
+
+try:
+    from lxml import etree
+except ImportError:
+    print 'installing lxml will speed this up'
+    import xml.etree.ElementTree as etree
+
+schema = "http://www.cccbr.org.uk/methods/schemas/2007/05/methods"
+
+def xmlns(tag):
+    return "{%s}%s" % (schema, tag)
+
+def find_tag_text(element, tag):
+    text = getattr(element.find(xmlns(tag)), 'text', None)
+    return str(text.encode("utf-8")) if text else ''
+
+class Command(BaseCommand):
+    args = '<path to CCCBR xml file>'
+    help = 'Populates the methods table from the CCCBR method XML'
+
+    logging.basicConfig()
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
+
+    def handle(self, file, *args, **options):
+
+        if not os.path.isfile(file):
+            raise IOError('file %s not found' % file)
+
+        self.logger.debug('populating from file "%s"' % file)
+
+        tree = etree.parse(file)
+        root = tree.getroot()
+
+        for methodSet in root.findall(xmlns('methodSet')):
+
+            properties = methodSet.find(xmlns('properties'))
+
+            # create the MethodSet object first
+            ms = MethodSet(notes=find_tag_text(methodSet, 'notes'),
+                           p_stage=find_tag_text(properties, 'stage'),
+                           p_lengthOfLead=find_tag_text(properties, 'lengthOfLead'),
+                           p_numberOfHunts=find_tag_text(properties, 'numberOfHunts'),
+                           p_huntBellPath=find_tag_text(properties, 'huntBellPath'),
+                           p_symmetry=find_tag_text(properties, 'symmetry'))
+            ms.save()
+
+            self.logger.debug(u'MethodSet %s saved' % ms)
+
+            # pull out all the methods in this set
+            methods = methodSet.findall(xmlns('method'))
+
+            for method in methods:
+
+                # create method linking back to method set
+                m = Method(id=int(method.attrib['id'].split('id')[-1]),
+                           method_set=ms,
+                           name=find_tag_text(method, 'name'),
+                           classification=find_tag_text(method, 'classification'),
+                           notation=find_tag_text(method, 'notation'),
+                           title=find_tag_text(method, 'title'),
+                           leadHeadCode=find_tag_text(method, 'leadHead'),
+                           method_notes=find_tag_text(method, 'notes'))
+
+                m.save()
+
+                try: # can't be bothered to fix for Sméagol
+                    self.logger.debug(u'Method {method} saved'.format(method=m.name))
+                except UnicodeEncodeError:
+                    print 'Unicode{De,En}codeError - probably "Sméagol"'
+                    pass
+                except UnicodeDecodeError:
+                    print 'Unicode{De,En}codeError - probably "Sméagol"'
+                    pass
+
+                performances = method.find(xmlns('performances'))
+                references = method.find(xmlns('references'))
+                falseness = method.findall(xmlns('falseness'))
+
+                # create performance (if any) and update method
+                if performances is not None:
+                    fhbp = performances.find(xmlns('firstHandbellPeal'))
+                    ftbp = performances.find(xmlns('firstTowerbellPeal'))
+
+                    if fhbp is not None:
+
+                        perf = FirstHandbellPeal(
+                            building=find_tag_text(fhbp, 'building'),
+                            town=find_tag_text(fhbp, 'town'),
+                            county=find_tag_text(fhbp, 'county'),
+                            region=find_tag_text(fhbp, 'region'),
+                            country=find_tag_text(fhbp, 'country'),
+                            location=find_tag_text(fhbp, 'location'),
+                            address=find_tag_text(fhbp, 'address'),
+                            date=find_tag_text(fhbp, 'date'))
+
+                        perf.save()
+                        m.first_hb_peal = perf
+                        m.save()
+
+                        self.logger.debug(u'Handbell Performance %s saved' % perf)
+
+                    if ftbp is not None:
+                        perf = FirstTowerbellPeal(
+                            building=find_tag_text(ftbp, 'building'),
+                            town=find_tag_text(ftbp, 'town'),
+                            county=find_tag_text(ftbp, 'county'),
+                            region=find_tag_text(ftbp, 'region'),
+                            country=find_tag_text(ftbp, 'country'),
+                            location=find_tag_text(ftbp, 'location'),
+                            address=find_tag_text(ftbp, 'address'),
+                            date=find_tag_text(ftbp, 'date'))
+
+                        perf.save()
+                        m.first_tb_peal = perf
+                        m.save()
+
+                        self.logger.debug(u'Towerbell Performance %s saved' % perf)
+
+                if references is not None:
+
+                    for reference in references:
+
+                        m.rw_reference = find_tag_text(reference, 'rwRef')
+                        m.bn_reference = find_tag_text(reference, 'bnRef')
+                        m.cb_reference = find_tag_text(reference, 'cbRef')
+                        m.pmm_reference = find_tag_text(reference, 'pmmRef')
+                        m.tdmm_reference = find_tag_text(reference, 'tdmmRef')
+
+                    m.save()
+
+                if falseness is not None:
+                    for fs in falseness:
+                        m.falseness_groups = find_tag_text(fs, 'fchGroups')
+                    m.save()
--- methods/models.py
+++ methods/models.py
@@ -2,84 +2,127 @@ import logging
 from django.db import models
 from django.core.urlresolvers import reverse
 
-from picklefield.fields import PickledObjectField
-
 from course import Course
 
 logger = logging.getLogger('django_debug')
 
+ORDER_CHOICES = (
+    ( 4, 'Minimus'),
+    ( 5, 'Doubles'),
+    ( 6, 'Minor'),
+    ( 7, 'Triples'),
+    ( 8, 'Major'),
+    ( 9, 'Caters'),
+    (10, 'Royal'),
+    (11, 'Cinques'),
+    (12, 'Maximus'),
+    (13, 'Sextuples'),
+    (14, 'Fourteen'),
+    (15, 'Septuples'),
+    (16, 'Sixteen'),
+)
 
-class MethodsStatus(models.Model):
-    """ Some information about when the method database was last updated etc. """
-    updated = models.DateField(unique=True)
-    num_methods = models.IntegerField()
-    orders = models.ManyToManyField('MethodOrderCount')
+#class MethodsStatus_old(models.Model):
+#    """ Some information about when the method database was last updated etc. """
+#    updated = models.DateField(unique=True)
+#    num_methods = models.IntegerField()
+#    orders = models.ManyToManyField('MethodOrderCount')
+#
+#    class Meta:
+#        ordering = ['updated']
+#        get_latest_by = 'updated'
+#
+#    def __str__(self):
+#        return unicode(self).encode('utf-8')
+#
+#    def __unicode__(self):
+#        return 'Update %s' % self.updated
 
-    class Meta:
-        ordering = ['updated']
-        get_latest_by = 'updated'
 
-    def __str__(self):
-        return unicode(self).encode('utf-8')
+#class MethodOrderCount_old(models.Model):
+#    """ Information about methods in database """
+#
+#    order = models.PositiveIntegerField(choices=ORDER_CHOICES)
+#    count = models.IntegerField(null=True)
+#    updated = models.DateField()
+#
+#    def get_absolute_url(self):
+#        return reverse('methods:order', args=[self.order,])
+#
+#    class Meta:
+#        ordering = ['order']
+#        get_latest_by = 'updated'
+#
+#    def __str__(self):
+#        return unicode(self).encode('utf-8')
+#
+#    def __unicode__(self):
+#        return '%d' % self.order
 
-    def __unicode__(self):
-        return 'Update %s' % self.updated
+
+#class Method_old(models.Model):
+#    """
+#    Simple method class - to hold all methods from that CSV file
+#    """
+#
+#    nbells = models.IntegerField('Number of bells', null=True)
+#    name = models.CharField('Name of the method', max_length=255)
+#    slug = models.SlugField(max_length=255)
+#    places = models.TextField('Place notation')
+#    calls = models.CharField('call type', max_length=8)
+#    nchanges = models.IntegerField('Number of changes (in a plain course)', null=True)
+#    nleadends = models.IntegerField('Number of lead ends (in a plain course)', null=True)
+#
+#    problem = models.BooleanField('Is there a problem with this entry?')
+#
+#    sanitised_notation = models.TextField('Sanitised Place notation')
+#    leadends = PickledObjectField('Leadends', null=True)
+#    changes = PickledObjectField('Changes', null=True)
 
 
-class MethodOrderCount(models.Model):
-    """ Information about methods in database """
-    ORDER_CHOICES = (
-        ( 4, 'Minimus'),
-        ( 5, 'Doubles'),
-        ( 6, 'Minor'),
-        ( 7, 'Triples'),
-        ( 8, 'Major'),
-        ( 9, 'Caters'),
-        (10, 'Royal'),
-        (11, 'Cinques'),
-        (12, 'Maximus'),
-        (13, 'Sextuples'),
-        (14, 'Fourteen'),
-        (15, 'Septuples'),
-        (16, 'Sixteen'),
-    )
+class MethodSet(models.Model):
+    """
+    MethodSet
+    """
 
-    order = models.PositiveIntegerField(choices=ORDER_CHOICES)
-    count = models.IntegerField(null=True)
-    updated = models.DateField()
+    notes = models.CharField('notes', max_length=255)
 
-    def get_absolute_url(self):
-        return reverse('methods:order', args=[self.order,])
-
-    class Meta:
-        ordering = ['order']
-        get_latest_by = 'updated'
-
-    def __str__(self):
-        return unicode(self).encode('utf-8')
-
-    def __unicode__(self):
-        return '%d' % self.order
-
+    p_stage = models.IntegerField('properties->stage')
+    p_lengthOfLead = models.IntegerField('properties->lengthOfLead')
+    p_numberOfHunts = models.IntegerField('properties->numberOfHunts')
+    p_huntBellPath = models.CharField('properties->huntBellPath', max_length=255)
+    p_symmetry = models.CharField('properties->symmetry', max_length=255)
 
 class Method(models.Model):
     """
-    Simple method class - to hold all methods from that CSV file
+    Method
     """
 
-    nbells = models.IntegerField('Number of bells', null=True)
-    name = models.CharField('Name of the method', max_length=255)
-    slug = models.SlugField(max_length=255)
-    places = models.TextField('Place notation')
-    calls = models.CharField('call type', max_length=8)
-    nchanges = models.IntegerField('Number of changes (in a plain course)', null=True)
-    nleadends = models.IntegerField('Number of lead ends (in a plain course)', null=True)
+    id = models.IntegerField('id', primary_key=True)
+    method_set = models.ForeignKey(MethodSet)
 
-    problem = models.BooleanField('Is there a problem with this entry?')
+    title = models.CharField('title', max_length=255, unique=True)
+    slug = models.SlugField('slug', max_length=255)
+    name = models.CharField('name', max_length=255)
+    notation = models.CharField('notation', max_length=255)
 
-    sanitised_notation = models.TextField('Sanitised Place notation')
-    leadends = PickledObjectField('Leadends', null=True)
-    changes = PickledObjectField('Changes', null=True)
+    # possible nulls
+    leadHeadCode = models.CharField('lead head code', max_length=16)
+    classification = models.CharField('classification', max_length=255)
+    method_notes = models.CharField('method notes', max_length=255)
+
+    # have own children in xml
+    falseness_groups = models.CharField('fchGroups', max_length=31)
+
+    rw_reference = models.CharField('"Ringing World" reference', max_length=31)
+    bn_reference = models.CharField('"The Bell News" reference', max_length=31)
+    cb_reference = models.CharField('"Church Bells" reference', max_length=31)
+    pmm_reference = models.CharField('Numerical index in the Plain Minor Methods collection', max_length=31)
+    tdmm_reference = models.CharField('Numerical index in the Treble Dodging Minor Methods collection', max_length=31)
+
+    # performances
+    first_hb_peal = models.OneToOneField('FirstHandbellPeal', related_name='first hand bell peak', null=True)
+    first_tb_peal = models.OneToOneField('FirstTowerbellPeal', related_name='first tower bell peak', null=True)
 
     def get_absolute_url(self):
         return reverse('methods:single_method', args=[self.slug,])
@@ -90,21 +133,41 @@ class Method(models.Model):
     def __unicode__(self):
         return self.name
 
-    def ring(self, save_changes=False):
-        self.course = Course(self.nbells, self.places, self.name)
-        self.course.ring()
-        
-        self.nleadends = self.course.nleadends
-        self.sanitised_notation = self.course.notation
-        self.nchanges = len(self.course.allChanges)
-
-        if save_changes:
-            self.changes = self.course.allChanges
-            self.leadends = self.course.leadends
-    
-        self.save()
-
-
+#    def ring(self, save_changes=False):
+#        self.course = Course(self.nbells, self.places, self.name)
+#        self.course.ring()
+#
+#        self.nleadends = self.course.nleadends
+#        self.sanitised_notation = self.course.notation
+#        self.nchanges = len(self.course.allChanges)
+#
+#        if save_changes:
+#            self.changes = self.course.allChanges
+#            self.leadends = self.course.leadends
+#
+#        self.save()
 
 
+
+class Performance(models.Model):
+    """
+    Performance - not well sanitised
+    """
+    date = models.DateField('date')
+    building = models.CharField('town', max_length=31)
+    town = models.CharField('town', max_length=31)
+    county = models.CharField('county', max_length=31)
+    region = models.CharField('region', max_length=31)
+    country = models.CharField('country', max_length=31)
+    address = models.CharField('address', max_length=31)
+    location = models.CharField('address', max_length=31)
+
+    class Meta:
+        abstract = True
+
+class FirstHandbellPeal(Performance):
+    pass
+
+class FirstTowerbellPeal(Performance):
+    pass
 
