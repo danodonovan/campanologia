303ada86a7c8bb59b06478aecd08bc44ba20dd82
Dan O'Donovan <danodonovan@users.noreply.github.com>
2013-01-30 21:55:17 +0000

    changes to support extra method views

--- methods/urls.py
+++ methods/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls.defaults import patterns, url
 
-from methods.views import method_view, method_set_view, method_sets_view, order_view, random_view
+from methods.views import method_view, method_set_view, method_sets_view, order_view, random_view, details_view
 
 urlpatterns = patterns('methods.urls',
     url(r'random/$', random_view, name='random'),
@@ -8,5 +8,6 @@ urlpatterns = patterns('methods.urls',
     url(r'order/(?P<order>.*)/$', order_view, name='order'),
     url(r'set/(?P<slug>.*)/$', method_set_view, name='method_set'),
     url(r'sets/$', method_sets_view, name='method_sets'),
+    url(r'(?P<slug>.*)/details$', details_view, name='detail_method'),
     url(r'(?P<slug>.*)/$', method_view, name='single_method'),
 )
