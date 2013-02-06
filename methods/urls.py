8cc9d02372d012b1b048a83910864bf6431e8dd6
Dan O'Donovan <danodonovan@users.noreply.github.com>
2013-02-06 23:12:06 +0000

    adding class based views for single method view optimising random method with caching tidied javascript up

--- methods/urls.py
+++ methods/urls.py
@@ -1,14 +1,29 @@
 from django.conf.urls.defaults import patterns, url
 
-from methods.views import method_view, method_set_view, method_sets_view, order_view, order_list_view, random_view, details_view
+from .views import method_set_view, method_sets_view, order_view, order_list_view, random_view
+from .views import MethodView, MethodInfoView, RandomMethodView
 
 urlpatterns = patterns('methods.urls',
-    url(r'random/$', random_view, name='random'),
+    # url(r'random/$', random_view, name='random'),
     url(r'random/(?P<order>.*)/$', random_view, name='random_with_order'),
     url(r'order/(?P<order>.*)/$', order_view, name='order'),
     url(r'order/$', order_list_view, name='order_list'),
     url(r'set/(?P<slug>.*)/$', method_set_view, name='method_set'),
     url(r'sets/$', method_sets_view, name='method_sets'),
-    url(r'(?P<slug>.*)/details$', details_view, name='detail_method'),
-    url(r'(?P<slug>.*)/$', method_view, name='single_method'),
+    # class based views
+    url(
+        regex=r'random/',
+        view=RandomMethodView.as_view(),
+        name='random'
+    ),
+    url(
+        regex=r'(?P<slug>.*)/$',
+        view=MethodView.as_view(),
+        name='single_method'
+    ),
+    url(
+        regex=r'(?P<slug>.*)/details',
+        view=MethodInfoView.as_view(),
+        name='detail_method'
+    ),
 )
