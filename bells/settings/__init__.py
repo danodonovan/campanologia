11e1a343a3cf7a829cde9ec6bda6b0b304a08d68
Dan O'Donovan <danodonovan@users.noreply.github.com>
2013-12-24 22:34:36 +0000

    trying to fix DATABASES

--- bells/settings/__init__.py
+++ bells/settings/__init__.py
@@ -117,7 +117,9 @@ except ImportError:
 # heroku stuff
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
+if DATABASES is None:
+    DATABASES = {}
+DATABASES['default'] = dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
