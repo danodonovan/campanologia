9571e67465b0a5053f17fc4197443beb40580919
Dan O'Donovan <dan.odonovan@gmail.com>
2014-01-01 22:42:52 +0000

    humanize method count on homepage

--- bells/settings.py
+++ bells/settings.py
@@ -133,6 +133,7 @@ TEMPLATE_DIRS = (
 
 INSTALLED_APPS = (
     'django.contrib.auth',
+    'django.contrib.humanize',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.sites',
