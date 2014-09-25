6bf666b55851424e073325342b28eb08876107ed
Dan O'Donovan <d.odonovan@base4.co.uk>
2014-09-25 12:26:23 +0100

    sitemap rejig to include caching and new MethodSet views

--- bells/sitemap.py
+++ bells/sitemap.py
@@ -1,65 +1,19 @@
 #! /usr/bin/env python
-import os
-from datetime import datetime
+# -*- coding: utf-8 -*-
+from __future__ import division, print_function, absolute_import
 
-from django.conf import settings
-from django.contrib.sitemaps import Sitemap
-
+from django.contrib.sitemaps import GenericSitemap
 from methods.models import Method, MethodSet
 
+method_dict = {
+    'queryset': Method.objects.all(),
+}
 
-class StaticSitemap(Sitemap):
-    priority = 0.5
-
-    # set lastmod to the mod time of the last file changed in the template dir
-    lastmod = datetime.fromtimestamp(0)
-    for template_dir in settings.TEMPLATE_DIRS:
-        for root, dirs, files in os.walk(template_dir):
-            for file in files:
-                filepath = os.path.join(root, file)
-                newmod = datetime.fromtimestamp(os.stat(filepath).st_mtime)
-                lastmod = newmod if newmod > lastmod else lastmod
-
-    def items(self):
-        return [
-            "/",
-            "/about",
-        ]
-
-    def location(self, obj):
-        return obj[0] if isinstance(obj, tuple) else obj
-
-    def changefreq(self, obj):
-        return obj[1] if isinstance(obj, tuple) else "monthly"
-
-
-class BaseSitemap(Sitemap):
-    priority = 0.5
-    changefreq = "monthly"
-
-    def location(self, obj):
-        return obj.get_absolute_url()
-
-        # def lastmod(self, obj):
-        # return MethodSet.objects.latest().updated
-
-
-class MethodSitemap(BaseSitemap):
-    changefreq = "monthly"
-
-    def items(self):
-        return Method.objects.all()
-
-
-class MethodOrderCountSitemap(BaseSitemap):
-    changefreq = "monthly"
-
-    def items(self):
-        return MethodSet.objects.all()
-
+method_set_dict = {
+    'queryset': MethodSet.objects.all(),
+}
 
 sitemaps = {
-    'static': StaticSitemap,
-    'methods': MethodSitemap,
-    # 'order': MethodOrderCount,
+    'methods': GenericSitemap(method_dict, priority=0.6, changefreq='never'),
+    'method_sets': GenericSitemap(method_set_dict, priority=0.6, changefreq='never'),
 }
--- bells/urls.py
+++ bells/urls.py
@@ -1,12 +1,26 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import division, print_function, absolute_import
+
 from django.conf.urls import patterns, include, url
-from django.contrib.sitemaps.views import sitemap as sitemap_view
-from bells.views import home, about
-from bells.sitemap import sitemaps
+from django.contrib.sitemaps import views as sitemaps_views
+from django.views.decorators.cache import cache_page
+
+from .views import home, about
+from .sitemap import sitemaps
 
 urlpatterns = patterns('',
     url(r'^$', home, name='home'),
     url(r'^about/', about, name='about'),
     url(r'^methods/', include('methods.urls', namespace='methods', app_name='methods')),
-    # sitemap for SEO
-    url(r'^sitemap\.xml$', sitemap_view, {'sitemaps': sitemaps}, name='sitemap')
+)
+
+# sitemap bits - now cached as sitemap was HUGE and slow...
+urlpatterns += patterns('django.contrib.sitemaps.views',
+    url(r'^sitemap\.xml$',
+        cache_page(86400)(sitemaps_views.index),
+        {'sitemaps': sitemaps, 'sitemap_url_name': 'sitemaps'}),
+    url(r'^sitemap-(?P<section>.+)\.xml$',
+        cache_page(86400)(sitemaps_views.sitemap),
+        {'sitemaps': sitemaps}, name='sitemaps'),
 )
