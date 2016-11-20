59b273a002dcb8512292c9703823bd8cb5fc2ea1
Dan O'Donovan <dan.odonovan@gmail.com>
2016-11-20 21:22:04 +0000

    remove unused settings

--- bells/settings/__init__.py
+++ bells/settings/__init__.py
@@ -1,13 +1,4 @@
-#! /usr/bin/env python
-# -*- coding: utf-8 -*-
-from __future__ import division, print_function, absolute_import
-import socket
+import os
+from .default import *
 
-from .settings import *
-
-# get local files
-if socket.gethostname().lower() in ('odonovan-pc', 'probeast.local', 'probeast.home'):
-    from .host_odonovanpc import *
-
-if socket.gethostname().lower() == 'dodPersonal2':
-    from .host_dodPersonal2 import *
+DEBUG = True if os.getenv('DEBUG', None) is not None else DEBUG
