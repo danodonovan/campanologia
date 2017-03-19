952f19540ed3869d3504b40377a314c716f1eabb
Dan O'Donovan <dan.odonovan@gmail.com>
2017-03-19 20:14:24 +0000

    Simplify settings

--- bells/settings.py
+++ bells/settings.py
@@ -4,7 +4,7 @@ from __future__ import division, print_function, absolute_import
 import os
 
 # Django settings for project.
-DEBUG = False
+DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
--- bells/settings/__init__.py
+++
@@ -1,4 +0,0 @@
-import os
-from .default import *
-
-DEBUG = True if os.getenv('DEBUG', None) is not None else DEBUG
