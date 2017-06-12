a4ad5c2ad41f8e22cc5a9e9bf2c2b4e4a1ea0a45
Dan O'Donovan <dan.odonovan@gmail.com>
2017-06-12 21:49:40 +0000

    CI fix - missing environment variables

--- bells/settings.py
+++ bells/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', False)
 
-ALLOWED_HOSTS = os.getenv('HOST').split(',')
+ALLOWED_HOSTS = os.getenv('HOST', '').split(',')
 
 
 # Application definition
