1a9351407845891ce430c1193aa510a60e44966c
Dan O'Donovan <dan.odonovan@gmail.com>
2017-08-29 16:14:41 +0100

    Add sitemap URL testing

--- methods/tests/bells/test_urls.py
+++ methods/tests/bells/test_urls.py
@@ -25,3 +25,23 @@ class TestBellsUrls(TestCase):
     def test_search(self):
         response = self.client.get('/search')
         self.assertEqual(response.status_code, 301)
+
+
+class TestSiteMapUrls(TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        super(TestSiteMapUrls, cls).setUpClass()
+        cls.client = Client()
+
+    def test_sitemap(self):
+        response = self.client.get('/sitemap.xml')
+        self.assertEqual(response.status_code, 200)
+
+    def test_sitemap_methodsets(self):
+        response = self.client.get('/sitemap-method_sets.xml')
+        self.assertEqual(response.status_code, 200)
+
+    def test_sitemap_methods(self):
+        response = self.client.get('/sitemap-methods.xml')
+        self.assertEqual(response.status_code, 200)
