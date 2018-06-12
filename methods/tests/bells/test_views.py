16fc58b986bbf483b773df41f3f2b16015fa0592
Dan O'Donovan <dan.odonovan@gmail.com>
2018-06-12 22:25:16 +0100

    Add bells.views tests

--- methods/tests/bells/test_views.py
+++ methods/tests/bells/test_views.py
@@ -4,8 +4,6 @@ from django.urls import reverse
 
 class TestBellsViews(TestCase):
 
-    fixtures = ['methods.json', 'methodset.json', 'firsttowerbellpeal.json']
-
     @classmethod
     def setUpClass(cls):
         super(TestBellsViews, cls).setUpClass()
@@ -20,3 +18,17 @@ class TestBellsViews(TestCase):
         with self.settings(DEBUG=False):
             response = self.client.get(reverse(viewname='home'))
             self.assertNotContains(response, 'Development Site')
+
+    def test_home(self):
+        response = self.client.get(reverse(viewname='home'))
+        self.assertContains(
+            response,
+            'Bell ringers sometimes need blue lines quickly, this site aims to provide just that.'
+        )
+
+    def test_about(self):
+        response = self.client.get(reverse(viewname='about'))
+        self.assertContains(
+            response,
+            'This little site has been created as an exercise in web site building, javascript, django and python.'
+        )
